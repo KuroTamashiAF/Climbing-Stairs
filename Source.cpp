49becf1d44840f4a373eec4590b04faa9885b784
Fedorov <chugun777akk@mail.ru>
2023-04-09 10:37:14 +0700

    create project

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+/*You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?*/
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+
+    }
+};
+
+int main()
+{
+    Solution x;
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+    cout << x.climbStairs(num) << endl;
+    return 0;
+}
